0f50033f37aa920c2c059d463b99d2e8a900343b
Ivan <kyurkchyu10@gmail.com>
2023-03-26 10:16:36 +0300

    Interfaces

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import Card from './Card'
 import './Content.css'
-import {ICar} from "../data";
+import {IAddedCar} from "../data";
 
 
 export default function Content({data}:any) {
 
     return (
         <div className="content d-flex flex-wrap gap-4 justify-content-center">
-            {data.map((el:ICar)=><Card key={el.descrip} {...el}/>)}
+            {data.map((el:IAddedCar)=><Card key={el.descrip} {...el}/>)}
         </div>
     );
 }
